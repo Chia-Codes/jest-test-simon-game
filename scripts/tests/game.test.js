0b4f61cd8ebf3839b20203dbd66e18fda67b7cfd
Chia-Codes <sanchian-l@hotmail.co.uk>
2025-05-11 11:36:15 +0100

    Setup tests for functions in game

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,8 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { game } = require("../game");
 
 // Code to for beginning of every project
 beforeAll(() => {
@@ -7,9 +12,21 @@ beforeAll(() => {
 })
 
 // For setting up and tearing down test environments
-//Fail test to see if score exsists
+//Fail test first, then fix it
 describe("game object contains correct keys", () => {
-    test ("score key exists", () => {
+    test("score key exists", () => {
         expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
